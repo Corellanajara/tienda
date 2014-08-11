e6712a551d51a2c692c32d55c4d4e717788814aa
Sergio Pérez <sergio.perez@albatronic.com>
2014-08-12 01:23:53 +0200

    Commit Global

--- home.php
+++ home.php
@@ -2,9 +2,15 @@
 /* Redirecciona a una página diferente en el mismo directorio el cual se hizo la petición */
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$extra = 'index.php';
+
+if ($_SERVER['PHP_SELF'] == '/home.php') {
+    $extra = "index.php";
+} else {
+    $extra = "carton.html";
+}
 header("Location: http://$host$uri/$extra");
+//echo ("Location: http://$host$uri/$extra");
 exit;
-?>
+
 
 
