e707886513c68881b65e7ebd8fc926d5b6160b85
Sergio Pérez <sergio.perez@albatronic.com>
2014-09-13 03:07:39 +0200

    Ajustes paypal

--- entities/abstract/TiposTpv.class.php
+++ entities/abstract/TiposTpv.class.php
@@ -14,7 +14,6 @@ class TiposTpv extends Tipos {
         array('Id' => '2', 'Value' => 'Redsys/Sermepa',),
         array('Id' => '3', 'Value' => 'Pagantis',),
     );
-    
     static $urlTpv = array(
         //Paypal
         '1' => array(
@@ -75,9 +74,7 @@ class TiposTpv extends Tipos {
 
         $pedido = new PedidosWebCab($idPedido);
         $total = $pedido->getTotal() + $pedido->getGastosEnvio();
-        //$nItems = $pedido->getNItems();
-        unset($pedido);
-                 
+
         $modo = ($_SESSION['varEnv']['Pro']['shop']['paypal']['modo'] == '1') ? 'real' : 'test';
 
         $parametros = array(
@@ -91,9 +88,30 @@ class TiposTpv extends Tipos {
             'notify_url' => $_SESSION['varEnv']['Pro']['shop']['url'] . '/lib/notificacionPaypal.php',
             'amount_1' => number_format($total, 2, '.', ''), // solo 2 decimales y sin separador de miles
             'item_name_1' => 'N. de Pedido:  ' . $idPedido,
-            //'quantity_1' => number_format($nItems,0), // Se debe expresar como un entero y debe ser > 0
             'custom' => $idPedido,
+            // Dirección de entrega
+            'first_name' => $pedido->getIDCliente()->getRazonSocial(),
+            'address1' => $pedido->getIDDirec()->getDireccion(),
+            'city' => $pedido->getIDDirec()->getIDPoblacion()->getMunicipio(),
+            'state' => $pedido->getIDDirec()->getIDProvincia()->getProvincia(),
+            'zip' => $pedido->getIDDirec()->getCodigoPostal(),
+            'country' => $pedido->getIDDirec()->getIDPais()->getCodigo(),
+            'shipping_1' => $pedido->getGastosEnvio(),
+            'address_override' => 1,
+            'cpp_cart_border_color' => '333333',
+            //'cpp_header_image' => "https://URL AL LOGO DE LA TIENDA",           
         );
+
+        // Incluyo las líneas de pedido
+        $l = 1;
+        foreach ($pedido->getLineas() as $linea) {
+            $l = $l + 1;
+            $parametros["item_name_{$l}"] = $linea->getDescripcion();
+            $parametros["quantity_{$l}"] = number_format($linea->getUnidades(),0);
+            $parametros["amount_{$l}"] = $linea->getImporte();
+        }
+
+        unset($pedido);
 
         return $parametros;
     }
@@ -106,7 +124,7 @@ class TiposTpv extends Tipos {
      * @return array
      */
     static private function getParamsRedsys($idPedido) {
-        
+
         $pedido = new PedidosWebCab($idPedido);
         $total = $pedido->getTotal() + $pedido->getGastosEnvio();
         unset($pedido);
@@ -120,10 +138,10 @@ class TiposTpv extends Tipos {
         if ($total[0] == '0') {
             // si es menor de 1 hay q quitar el cero inicial (ej: 0.25 => 025 => 25)
             $total = substr($total, 1);
-        } 
-        
+        }
+
         $modo = ($_SESSION['varEnv']['Pro']['shop']['redsys']['modo'] == '1') ? 'real' : 'test';
-        
+
         $parametros = array(
             'url_tpv' => self::$urlTpv[2][$modo],
             'Ds_Merchant_Amount' => $total,
@@ -152,9 +170,9 @@ class TiposTpv extends Tipos {
      * @param int $idPedido
      * @param decimal $total
      * @return array
-     */    
+     */
     static private function getParamsPagantis($idPedido) {
-        
+
         $pedido = new PedidosWebCab($idPedido);
         $total = $pedido->getTotal() + $pedido->getGastosEnvio();
         unset($pedido);
@@ -168,10 +186,10 @@ class TiposTpv extends Tipos {
         if ($total[0] == '0') {
             // si es menor de 1 hay q quitar el cero inicial (ej: 0.25 => 025 => 25)
             $total = substr($total, 1);
-        }  
-        
+        }
+
         $modo = ($_SESSION['varEnv']['Pro']['shop']['pagantis']['modo'] == '1') ? 'real' : 'test';
-        
+
         $parametros = array(
             'url_tpv' => self::$urlTpv[3][$modo],
             'amount' => $total,
@@ -182,7 +200,7 @@ class TiposTpv extends Tipos {
             'account_id' => $_SESSION['varEnv']['Pro']['shop']['pagantis']['codigo'],
             'ok_url' => $_SESSION['varEnv']['Pro']['shop']['url'] . '/carrito/notificacion/pagantis/ok',
             'nok_url' => $_SESSION['varEnv']['Pro']['shop']['url'] . '/carrito/notificacion/pagantis/ko',
-            'signature' => self::getSignaturePagantis($idPedido,$total),
+            'signature' => self::getSignaturePagantis($idPedido, $total),
             'locale' => $_SESSION['varEnv']['Pro']['shop']['pagantis']['idioma'],
         );
 
@@ -196,8 +214,8 @@ class TiposTpv extends Tipos {
      * @param string $total
      * @return string Firma sha1 en mayúsculas
      */
-    static function getSignatureRedsys($idPedido,$total) {
-        
+    static function getSignatureRedsys($idPedido, $total) {
+
         $modo = ($_SESSION['varEnv']['Pro']['shop']['redsys']['modo'] == '1') ? 'real' : 'test';
         $urlWeb = $_SESSION['varEnv']['Pro']['shop']['url'];
         $clave = ($modo === 'real') ?
@@ -210,11 +228,11 @@ class TiposTpv extends Tipos {
                 $_SESSION['varEnv']['Pro']['shop']['redsys']['moneda'] .
                 $_SESSION['varEnv']['Pro']['shop']['redsys']['tipoTransaccion'] .
                 $urlWeb . '/lib/notificacionRedsys.php' .
-                $clave; 
+                $clave;
 
-        return strtoupper(sha1($message))  ;      
+        return strtoupper(sha1($message));
     }
-    
+
     /**
      * Calcula la firma de Pagantis
      * 
@@ -222,8 +240,8 @@ class TiposTpv extends Tipos {
      * @param string $total
      * @return string Firma sha1
      */
-    static function getSignaturePagantis($idPedido,$total) {
-        
+    static function getSignaturePagantis($idPedido, $total) {
+
         $modo = ($_SESSION['varEnv']['Pro']['shop']['pagantis']['modo'] == '1') ? 'real' : 'test';
         $urlWeb = $_SESSION['varEnv']['Pro']['shop']['url'];
         $clave = ($modo === 'real') ?
@@ -237,8 +255,9 @@ class TiposTpv extends Tipos {
                 $_SESSION['varEnv']['Pro']['shop']['pagantis']['moneda'] .
                 'SHA1' .
                 $urlWeb . '/carrito/notificacion/pagantis/ok' .
-                $urlWeb . '/carrito/notificacion/pagantis/ko'; 
-        
+                $urlWeb . '/carrito/notificacion/pagantis/ko';
+
         return sha1($message);
     }
+
 }
